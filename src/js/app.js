df0a6c39d113d896d44032893f8d6fd7d7960d1e
MariellaNik <118596924+MariellaNik@users.noreply.github.com>
2023-01-24 16:16:22 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,10 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
+const products = document.querySelectorAll(".product");
+for(let product of products){
+  const price = product.querySelector(".price").innerText;
+  product.setAttribute("data-price", price);
+}
 
 });
